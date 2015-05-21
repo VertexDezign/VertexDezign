6c601e12666c287aae92c5d3980e526e9665038e
Stijn Wopereis <stijnwop@gmail.com>
2015-05-21 21:47:27 +0200

    Last for today.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="container">    
     <article style="float:left;width:75%;">
-        <h2><a id="watisglasvezel">News article</a></h2>        
+        <h2><a id="">News article</a></h2>        
         <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
-        <h2><a id="watisglasvezel">News article</a></h2>        
+        <h2><a id="">News article</a></h2>        
         <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
-        <h2><a id="watisglasvezel">News article</a></h2>        
+        <h2><a id="">News article</a></h2>        
         <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
-        <h2><a id="watisglasvezel">News article</a></h2>        
+        <h2><a id="">News article</a></h2>        
         <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
     </article>
     <section class="four">
@@ -18,9 +18,9 @@
                 <p>Latest downloads</p>
             </li>
             <ul style="list-style:none;padding:0;margin:0;">
-                <li><a class="green" href="#" onclick="scroll('#watisglasvezel');">Download entry</a></li>
-                <li><a class="green" href="#" onclick="scroll('#waaromglasvezel');">Download entry</a></li>
-                <li><a class="green" href="#" onclick="scroll('#waaromglas');">Download entry</a></li>
+                <li><a class="green" href="#" onclick="scroll('#');">Download entry</a></li>
+                <li><a class="green" href="#" onclick="scroll('#');">Download entry</a></li>
+                <li><a class="green" href="#" onclick="scroll('#');">Download entry</a></li>
             </ul>            
         </ul>
     </section>
