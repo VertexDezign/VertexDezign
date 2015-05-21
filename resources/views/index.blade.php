866042bb1b6417710de6c061bc70e9f561fedcab
Stijn Wopereis <stijnwop@gmail.com>
2015-05-21 21:04:56 +0200

    Initial implementation.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layout/master')
+
+@section('content')
+<div class="container">    
+    <article style="float:left;width:75%;">
+        <h2><a id="watisglasvezel">News article</a></h2>        
+        <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
+        <h2><a id="watisglasvezel">News article</a></h2>        
+        <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
+        <h2><a id="watisglasvezel">News article</a></h2>        
+        <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
+        <h2><a id="watisglasvezel">News article</a></h2>        
+        <p>This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text. This is the article text.</p>
+    </article>
+    <section class="four">
+        <ul style="list-style:none;padding:0;margin:0;">
+            <li>
+                <p>Latest downloads</p>
+            </li>
+            <ul style="list-style:none;padding:0;margin:0;">
+                <li><a class="green" href="#" onclick="scroll('#watisglasvezel');">Download entry</a></li>
+                <li><a class="green" href="#" onclick="scroll('#waaromglasvezel');">Download entry</a></li>
+                <li><a class="green" href="#" onclick="scroll('#waaromglas');">Download entry</a></li>
+            </ul>            
+        </ul>
+    </section>
+    <div style="clear:both;"></div>
+</div> 
+@endsection
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>     
+        <link href="{{ asset('/css/index.css') }}" rel="stylesheet">        
+    </head>
+    <body>
+        <nav>
+          <div class="container">
+              <a href="{{{URL('/')}}}" class="title"><img src="{{URL('/images/vertexDezign_logo.png')}}"/></a>
+              <div class="menu-holder">
+            <ul class="controllers">
+              @if (! \Auth::check())
+                    <li><a href="{{{URL('/news')}}}">News</a></li>
+              @endif
+                    <li><a href="{{{URL('/projects')}}}">Projects</a></li>
+                    <li><a href="{{{URL('/downloads')}}}">Downloads</a></li>
+              @if (! \Auth::check())
+                    <li><a href="{{{URL('/login')}}}">Login</a></li>
+              @else
+                    <li><a href="{{{URL('/dashboard')}}}">Dashboard</a></li>
+                    <li><a href="{{{URL('/logout')}}}">Logout</a></li>
+              @endif
+                  </ul>
+                  <div style="clear:both;"></div>
+              </div>
+              <div class="mobile-menu-toggler"></div>
+          </div>
+        </nav>
+        <div style="height:15px;background:#2E3539;"></div>
+          @yield('content')
+        <footer>          
+        </footer>
+    </body>
+</html>
