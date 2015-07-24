98001ca4992a21249214a2958d0deecb80e4641a
Stijn Wopereis <stijnwop@gmail.com>
2015-07-24 18:22:57 +0200

    Backend master

--- resources/views/layout/backend.blade.php
+++ resources/views/layout/backend.blade.php
@@ -182,56 +182,42 @@ use App\News;
     </script>
 </head>
 <body>
-<nav>
-    <div style="height:60px;background-color:#0AA699;">
-        <a href="{{URL('/backend/')}}"><h1><span>Vertex</span>backend</h1></a>
-    </div>
-    <ul id="menu" style="list-style:none;height:100%;">
-        <li><a href="{{URL('/backend/')}}"><img src="{{URL('/images/backend/menu/dashboard.png')}}"/><p>Dashboard</p></a></li>
-        <li><a href="{{URL('/backend/news')}}"><img src="{{URL('/images/backend/menu/partners.png')}}"/><p>WIP</p></a></li>
-        <li><a href="{{URL('/backend/news')}}"><img src="{{URL('/images/backend/menu/partners.png')}}"/><p>Todo's</p></a></li>
-        @if($permission == 'admin')
-        <li>
-            <a href="{{URL('/backend/news')}}"><img src="{{URL('/images/backend/menu/news.png')}}"/><p>News</p></a>
-        </li>
-        <li>
-            <a href="{{URL('/backend/projects')}}"><img src="{{URL('/images/backend/menu/projects.png')}}"/><p>Projects</p></a>
-        </li>
-        <li>
-            <a href="{{URL('/backend/downloads')}}"><img src="{{URL('/images/backend/menu/downloads.png')}}"/><p>Downloads</p></a>
-        </li>
-        <li>
-            <a href="{{URL('/backend/media')}}"><img src="{{URL('/images/backend/menu/media.png')}}"/><p>Media</p></a>
-        </li>
-        <li>
-            <a href="{{URL('/backend/slider')}}"><img src="{{URL('/images/backend/menu/media.png')}}"/><p>Slider</p></a>
-        </li>
-        <li>
-            <a href="{{URL('/backend/partner')}}"><img src="{{URL('/images/backend/menu/media.png')}}"/><p>Partner</p></a>
-        </li>
-        @endif
-    </ul>
-</nav>
-<div class="container">
-    <header>
-        <button onclick="toggleSideMenu()"><img src="{{URL('/images/backend/menu-alt.png')}}" /></button>
-        <button onclick="window.location.href='{{URL('/')}}'"><img src="{{URL('/images/backend/home.png')}}" /></button>
-        <img src="{{URL('/images/backend/search.png')}}" class="search-icon"/>
-        <input type="search" placeholder="Search in admin panel" />
-        <button style="float:right;margin-right:5px;" class="rotate" onclick="toggleMenu('settings-menu');"><img src="{{URL('/images/backend/settings.png')}}" /></button>
-        <button style="float:right;margin-right:5px;"><img src="{{URL('/images/backend/bell.png')}}" /></button>
-        <ul class="dropdown" id="settings-menu">
-            <li><a>My Account</a></li>
-            <li><a>Settings</a></li>
-            <li style="border-top:1px solid #e4e4e4;"><a href="{{URL('/logout')}}">Logout</a></li>
+    <nav>
+        <div style="height:60px;background-color:#0AA699;">
+            <a href="{{URL('/backend/')}}"><h1><span>Vertex</span>backend</h1></a>
+        </div>
+        <ul id="menu" style="list-style:none;height:100%;">
+            <li><a href="{{URL('/backend/')}}"><img src="{{URL('/images/backend/menu/dashboard.png')}}"/><p>Dashboard</p></a></li>
+            <li><a href="{{URL('/backend/news')}}"><img src="{{URL('/images/backend/menu/partners.png')}}"/><p>WIP</p></a></li>
+            <li><a href="{{URL('/backend/news')}}"><img src="{{URL('/images/backend/menu/partners.png')}}"/><p>Todo's</p></a></li>
+            @if($permission == 'admin')
+                <li><a href="{{URL('/backend/news')}}"><img src="{{URL('/images/backend/menu/news.png')}}"/><p>News</p></a></li>
+                <li><a href="{{URL('/backend/projects')}}"><img src="{{URL('/images/backend/menu/projects.png')}}"/><p>Projects</p></a></li>
+                <li><a href="{{URL('/backend/downloads')}}"><img src="{{URL('/images/backend/menu/downloads.png')}}"/><p>Downloads</p></a></li>
+                <li><a href="{{URL('/backend/media')}}"><img src="{{URL('/images/backend/menu/media.png')}}"/><p>Media</p></a></li>
+                <li><a href="{{URL('/backend/slider')}}"><img style="transform:scaleY(-1);" src="{{URL('/images/backend/menu/media.png')}}"/><p>Slider</p></a></li>
+                <li><a href="{{URL('/backend/partner')}}"><img src="{{URL('/images/backend/menu/partners.png')}}"/><p>Partner</p></a></li>
+            @endif
         </ul>
-
-        <?php
-        echo '<a style="float:right;margin-top:20px;margin-right:5px;">'.Auth::user()->firstname . ' <strong>' . Auth::user()->surname.'</strong></a>';
-        ?>
-
-    </header>
-    @yield('content')
-</div>
+    </nav>
+    <div class="container">
+        <header>
+            <button onclick="toggleSideMenu()"><img src="{{URL('/images/backend/menu-alt.png')}}" /></button>
+            <button onclick="window.location.href='{{URL('/')}}'"><img src="{{URL('/images/backend/home.png')}}" /></button>
+            <img src="{{URL('/images/backend/search.png')}}" class="search-icon"/>
+            <input type="search" placeholder="Search in admin panel" />
+            <button style="float:right;margin-right:5px;" class="rotate" onclick="toggleMenu('settings-menu');"><img src="{{URL('/images/backend/settings.png')}}" /></button>
+            <button style="float:right;margin-right:5px;"><img src="{{URL('/images/backend/bell.png')}}" /></button>
+            <ul class="dropdown" id="settings-menu">
+                <li><a>My Account</a></li>
+                <li><a>Settings</a></li>
+                <li style="border-top:1px solid #e4e4e4;"><a href="{{URL('/logout')}}">Logout</a></li>
+            </ul>
+            <?php
+            echo '<a style="float:right;margin-top:20px;margin-right:5px;">'.Auth::user()->firstname . ' <strong>' . Auth::user()->surname.'</strong></a>';
+            ?>
+        </header>
+        @yield('content')
+    </div>
 </body>
 </html>
