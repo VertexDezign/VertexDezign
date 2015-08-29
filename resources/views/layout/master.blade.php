37d7fa214391c5f4e498c793a6240debb4374b2f
Grisu118 <grisu118@grisu118.ch>
2015-08-29 16:32:19 +0200

    added Cookie Warning

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,6 +12,15 @@
         <!-- Title Meta -->
         <title>@yield('title') - VertexDezign</title>
         <meta name="description" content="@yield('description')">
+
+        <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+        <script type="text/javascript">
+            window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"http://vertexdezign.net/forum/index.php/CookiePolicy/","theme":"light-bottom"};
+        </script>
+
+        <script type="text/javascript" src="//s3.amazonaws.com/cc.silktide.com/cookieconsent.latest.min.js"></script>
+        <!-- End Cookie Consent plugin -->
+
     </head>
     <body>
         <nav>
