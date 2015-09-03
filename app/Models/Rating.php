45fb811ea71c5774b3cd288049f96389cc0fdfc0
Benjamin Leber <grisu118@grisu118.ch>
2015-09-03 20:27:05 +0200

    DB for Rating

---
+++ app/Models/Rating.php
@@ -0,0 +1,8 @@
+<?php namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model {
+
+    protected $table = 'rating';
+    protected $fillable = ['value', 'downloadId', 'WbbUId'];
+}
---
+++ database/migrations/2015_09_03_182135_create_rating_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('rating', function(Blueprint $table)
+		{
+			$table->integer('value');
+			$table->unsignedInteger('downloadId');
+			$table->integer('WbbUId');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('rating');
+	}
+
+}
